dd740288e5085170f715accf9d8f25a6075eeeba
Aleks <mikitsik@gmail.com>
2019-04-08 10:24:44 +0300

    Add last changes. Fix ABC-size error

--- HW_03/Aleks_Mikitsik/aleks_mikitsik_hw_03_t_03.rb
+++ HW_03/Aleks_Mikitsik/aleks_mikitsik_hw_03_t_03.rb
@@ -1,31 +1,19 @@
-#  first solution
-
-def task_3_1(message)
-  # str_res = []
-  int_res = []
-  message.each_line.map do |line|
+def cut(msge)
+  msge.each_line.map do |line|
     next unless line.include?('Calling core with action')
 
-    mark = line.match(/^\d{4}[\-\d{2}]*{2}\s[\d{2}\:]*{2}\d{2}\.\d/)
-    # str_res << mark
-    int_res << mark.to_s.delete('.', ':').match(/.{5}$/).to_s.to_i
+    line.match(/^\d{4}[\-\d{2}]*{2}\s[\d{2}\:]*{2}\d{2}\.\d/)
+        .to_s.delete('.').delete(':').match(/.{5}$/).to_s.to_i
   end
-  # first_num = int_res[1].round(-3) * 0.6 + (int_res[1] - int_res[1].round(-3))
-  # second_num = int_res[0].round(-3) * 0.6 + \
-  # (int_res[0] - int_res[0].round(-3))
-  # { }"(#{str_res[0]} - #{str_res[1]}) = (#{first_num} - #{second_num}) = \
-  # {(first_num - second_num) * 0.1}"
 end
 
-#  second one
+def filtered(msge)
+  cut(msge).reject(&:nil?).reverse \
+           .map { |x| x.round(-3) * 0.6 + x - x.round(-3) }
+end
 
-require 'time'
-def task_3_2(message)
-  date = []
-  message.each_line.map do |line|
-    next unless line.include?('Calling core with action')
+def task_03(msge)
+  return '0' if filtered(msge) == []
 
-    date << Time.strftime(line.match(/^.{21}/).to_s, '%M%S%L')
-  end
-  date
+  (filtered(msge).reduce(:-) * 0.1).to_s
 end
