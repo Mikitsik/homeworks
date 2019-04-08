1ccb5fe97fdd027377641d09723a67af4fa6e302
Aleks <mikitsik@gmail.com>
2019-04-08 16:11:47 +0300

    Add last fixes

--- HW_03/Aleks_Mikitsik/aleks_mikitsik_hw_03_t_01.rb
+++ HW_03/Aleks_Mikitsik/aleks_mikitsik_hw_03_t_01.rb
@@ -1,7 +1,7 @@
+def pre_task_1(message)
+  message.each_line.select { |line| line.chop!.match(/.*[e]rror/i) }
+end
+
 def task_1(message)
-  if message =~ /.*[e]rror/i
-    (message.each_line.select { |line| line.chop!.match(/.*[e]rror/i) }).to_s
-  else
-    ''
-  end
+  pre_task_1(message).join
 end
