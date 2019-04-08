e012082ed4d6827f20698bc56877512e4a5ba1bb
Aleks <mikitsik@gmail.com>
2019-04-08 16:18:33 +0300

    fix Rubocop complains

--- HW_03/Aleks_Mikitsik/aleks_mikitsik_hw_03_t_02.rb
+++ HW_03/Aleks_Mikitsik/aleks_mikitsik_hw_03_t_02.rb
@@ -4,7 +4,7 @@ def extract(message)
 
     "#{line.match(%r{\d{2}/[a-z]{3}/\d{4}[\:\d{2}]*{3}\s\+\d{4}}i)}" \
       ' FROM: ' + "#{line.match(/\d{2}\.\d.\d{3}\.\d{3}/)}" \
-      ' TO: ' + "#{line.upcase.match(%r{/[A-Z]*/\d/[A-Z]*})}"
+      ' TO: ' + line.upcase.match(%r{/[A-Z]*/\d/[A-Z]*}).to_s
   end
 end
 
