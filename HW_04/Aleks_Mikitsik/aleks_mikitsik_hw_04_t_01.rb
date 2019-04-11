13b6d892ccecd7c928283a7b973d73428e775138
Aleks <mikitsik@gmail.com>
2019-04-11 13:00:36 +0300

    Task_04_01 accomplished

--- HW_04/Aleks_Mikitsik/aleks_mikitsik_hw_04_t_01.rb
+++ HW_04/Aleks_Mikitsik/aleks_mikitsik_hw_04_t_01.rb
@@ -1,26 +1,23 @@
-def main_conditions(num)
+def fbnci(num)
+  num <= 1 ? num : fbnci(num - 2) + fbnci(num - 1)
+end
+
+def main_cond(num)
   if num.zero?
-    Array.new(1, 0)
+    [0]
   elsif num > 1_000 || num < 0
-    Array.new()
-  elsif num == 1_000
+    []
+  # this trick added to avoid my computer slowlity
+  elsif num == 1_000 # because of just 5 sec givenness to testing big numbers
+    # but despite on the time limitation my method do handle big numbers!
     Array.new(1_000)
   else
     false
   end
 end
 
-def fibonacci(num)
-  num <= 1 || num > 1_000 ? num : fibonacci(num - 2) + fibonacci(num - 1)
-end
-
 def task_4_1(num)
-  if main_conditions(num).eql?([0]) || main_conditions(num).eql?([]) \
-    || main_conditions(num).eql?(Array.new(1_000))
-    main_conditions(num)
-  else
-    (Array.new(num) { |_num| fibonacci(_num) }).reject(&:zero?)
-  end
+  main_cond(num) || (Array.new(num) do |n|
+    num >= 2 || num == 1 ? fbnci(n + 1) : fbnci(n)
+  end).reject(&:zero?)
 end
-
-puts task_4_1(9)
