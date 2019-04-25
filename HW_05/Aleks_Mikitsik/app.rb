cb7ae88374b7266db34fb8f6699ca06474819d67
Aleks <mikitsik@gmail.com>
2019-04-25 15:16:13 +0300

    rubocop compliance

--- HW_05/Aleks_Mikitsik/app.rb
+++ HW_05/Aleks_Mikitsik/app.rb
@@ -14,7 +14,8 @@ hw01 = mentor_petrov.create_hw(
   hw_number: 'hw_01',
   hw_tasks:
   { task_1_1: 'some task_1_1 contents', task_1_2: 'some task_1_2 contents',
-    task_1_3: 'some task_1_3 contents', task_1_4: 'some task_1_4 contents' })
+    task_1_3: 'some task_1_3 contents', task_1_4: 'some task_1_4 contents' }
+)
 
 mentor_petrov.subscribed_to!(sudent_ivanov)
 
@@ -23,7 +24,8 @@ sudent_ivanov.takes_hw(hw01)
 st_ivanov_hw_01_done = sudent_ivanov.complete_hw(
   solved_tasks:
   { task_1_1: 'answer_1_1', task_1_2: 'answer_1_2',
-    task_1_3: 'answer_1_3', task_1_4: 'answer_1_4' })
+    task_1_3: 'answer_1_3', task_1_4: 'answer_1_4' }
+)
 
 sudent_ivanov.submit_hw
 
